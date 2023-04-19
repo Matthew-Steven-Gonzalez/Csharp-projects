df69d38343f156e73a0619a0c196528630797385
Matthew Steven Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-04-18 21:53:06 -0400

    boss class edited

--- rpgGame/boss.cs
+++ rpgGame/boss.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace HallsOfWestWood
 {
-    class Enemy
+    class Boss
     {
         public string name;
         public int coins = rnd.Next(5,100);
         public int health = rnd.Next(10,25);
         public int damage = rnd.Next(2,4)
-        Enemy(string name)
+        Boss(string name)
         {
             Enemy.name = name;
         };
