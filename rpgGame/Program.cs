8a4fc655d8227e423b51939aa792ff7316ef4374
Matthew Steven Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-04-18 21:09:36 -0400

    rpgGame

--- rpgGame/Program.cs
+++ rpgGame/Program.cs
@@ -8,9 +8,25 @@ namespace HallsOfWestWood
 {
     class Program
     {
+        public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            
+            Start();
+        }
+
+
+
+        static void Start()
+        {
+            string lineBreak = "_________________________________________";
+            Console.WriteLine("Welcome to the halls of WestWood?...");
+            Console.WriteLine("What is your name hero?:  ");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine("The large wooden doors swing open to an empty banquet hall.");
+            Console.WriteLine("Wall are adorned with old paintings cracking from aging. Candles hung on the wall tease the dark maze like halls ahead. A choice is to be made... ")
+            Console.WriteLine(lineBreak);
+
+
         }
     }
-}
+} 
---
+++ rpgGame/player.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HallsOfWestWood
+{
+    class Player
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 15;
+        public int damage = 2;
+        public int armorValue = 0;
+        public int potions = 5;
+    }
+}
