b1608a9f8fa8c60d90f3f4b906b911cb55a504ac
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-04-25 15:03:09 -0400

    added game back

--- rpgGame/Program.cs
+++ rpgGame/Program.cs
@@ -10,22 +10,19 @@ namespace HallsOfWestWood
     {
         public static Player currentPlayer = new Player();
 
-        public static Enemy wereWolf1 = new Enemy();
-
-        public static Enemy wereWolf2 = new Enemy();
-
-        public static Enemy werewolf3 = new
-
         static void Main(string[] args)
         {
             Start();
+            Encounters.hallFight();
         }
 
 
 
         static void Start()
         {
+            string titleCard = "___________Halls_Of_WestWood_____________";
             string lineBreak = "_________________________________________";
+            string emptyBreak= "                                         ";
             Console.WriteLine("Welcome to the halls of WestWood?...");
             Console.WriteLine("What is your name hero?:  ");
             currentPlayer.name = Console.ReadLine();
@@ -36,15 +33,58 @@ namespace HallsOfWestWood
             }
 
             Console.Clear();
-            Console.WriteLine("You awaken on the cold floor of a large banquet hall.\nYour head is clouded and your vison blurred\nThe only thing you know for sure is your name is " + currentPlayer.name);
+            Console.WriteLine(titleCard);
             Console.WriteLine(lineBreak);
-            Console.WriteLine("A choice is to be made...  ");
+            Console.WriteLine("You awaken on the cold floor of a large banquet hall.\nYour head is clouded and your vison blurred\nThe only thing you know for sure is your name is " + currentPlayer.name);
             Console.WriteLine(lineBreak);
             Console.ReadKey();
             Console.Clear();
+            Console.WriteLine(titleCard);
+            Console.WriteLine(lineBreak);
+            Console.WriteLine("A choice is to be made...  ");
             Console.WriteLine("Ahead of you is 2 halls.");
-            Console.WriteLine("On the left is a dark hall with candles barely illuminating a neverending hall.")
+            Console.WriteLine("On the left is a dark hall with candles barely illuminating a never-ending hall.");
+            Console.WriteLine("On the right is a door with swords crossed overhead and what appears to be blood splater across the door.");
+            Console.WriteLine("Which route do you take...");
+            Console.WriteLine(lineBreak);
+            string choice1 = Console.ReadLine();
+            Console.Clear();
 
+            if(choice1 == "right")
+            {    
+                Console.WriteLine("You reach out for the cold steel of the handle on the door and tug.");
+                if(currentPlayer.keys >= 1)
+                {
+                    Console.WriteLine(titleCard);
+                    Console.WriteLine(lineBreak);
+                    Console.WriteLine("You insert the key you found into the door and turn...You hear a click...");
+                    Console.WriteLine(lineBreak);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine(titleCard);
+                    Console.WriteLine(lineBreak);
+                    Console.WriteLine("You see a keyhole , but currently have no keys...");
+                    Console.WriteLine(lineBreak);
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+            }
+            else if(choice1.ToLower() == "left")
+            {
+                Console.Clear();
+                    Console.WriteLine(titleCard);
+                    Console.WriteLine(lineBreak);
+                    Console.WriteLine("You slowly begin walking down the hall.");
+                    Console.WriteLine("a creaking noise ahead causes the hair to raise on your neck... It's about to get hairy...");
+                    Console.WriteLine(lineBreak);
+                    Console.ReadKey();
+                    Console.Clear();
+            }
             
 
 
--- rpgGame/player.cs
+++ rpgGame/player.cs
@@ -8,11 +8,14 @@ namespace HallsOfWestWood
 {
     class Player
     {
+        public string location;
         public string name;
         public int coins = 0;
         public int health = 15;
         public int damage = 2;
         public int armorValue = 0;
+        public int weaponValue = 1;
         public int potions = 5;
+        public int keys = 1;
     }
 }
