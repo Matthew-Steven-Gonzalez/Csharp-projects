6776af9b8e2f57ad1e423e3dc59cfa71d7727050
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-02 10:56:49 -0400

    updated "GUI"

--- MakeAChoice/MakeAChoice.cs
+++ MakeAChoice/MakeAChoice.cs
@@ -16,8 +16,12 @@ public class MakeAChoice
     {
         Random r = new Random();
 
-        Console.WriteLine("Hello, Welcome to choice...");
-        Console.WriteLine("How many items/choices are there?");
+        Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+        Console.WriteLine("||    Hello, Welcome to choice...    ||");
+        Console.WriteLine("|| How many items/choices are there? ||");
+        Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+        
+
 
         string UI = Console.ReadLine();
 
@@ -28,31 +32,70 @@ public class MakeAChoice
         switch(num)
         {
             case 1:
-                Console.WriteLine("A");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... A        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
             case 2:
-                Console.WriteLine("B");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... B        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
+
             break;
             case 3:
-                Console.WriteLine("C");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... C        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
+
             break;
             case 4:
-                Console.WriteLine("D");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... D        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
             case 5:
-                Console.WriteLine("E");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... E        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
+
             break;
             case 6:
-                Console.WriteLine("F");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... F        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
             case 7:
-                Console.WriteLine("G");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... G        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
             case 8:
-                Console.WriteLine("H");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... H        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
             case 9:
-                Console.WriteLine("I");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("||      The Universe Has Spoken:     ||");
+                Console.WriteLine("||       Your Choice is ... I        ||");
+                Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
+                Console.ReadKey();
             break;
         }
         }
