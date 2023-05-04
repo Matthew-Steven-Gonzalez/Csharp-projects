d5e8bcc3cdd28637babfa7a873664552f67c9084
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-04 16:34:49 -0400

    update console gui

--- rpgGame/Program.cs
+++ rpgGame/Program.cs
@@ -19,59 +19,87 @@ namespace HallsOfWestWood
 
         static void Start()
         {
-            string titleCard = "________________Halls_Of_WestWood__________________";
-            string lineBreak = "___________________________________________________";
-            string emptyBreak= "                                                   ";
-            Console.WriteLine("Welcome to the halls of WestWood?...");
-            Console.WriteLine("What is your name hero?:  ");
+            string titleCard1 = "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+            string titleCard2 = "||                                                        ||";
+            string titleCard3 = "||                    Halls Of WestWood                   ||";
+            string titleCard4 = "||                    ~~~~~~~~~~~~~~~~~                   ||";
+            string titleCard5 = "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+            Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||            Welcome to the halls of WestWood?...        ||");
+            Console.WriteLine("||                 What is your name hero?:               ||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard5);
             currentPlayer.name = Console.ReadLine();
 
             if(currentPlayer.name == "" )
             {
-                currentPlayer.name = " Wait...I don't remember...";
+                currentPlayer.name = " Wait,I don't remember.";
             }
 
             Console.Clear();
-            Console.WriteLine(titleCard);
-            Console.WriteLine(lineBreak);
-            Console.WriteLine("You awaken on the cold floor of a large banquet hall.\nYour head is clouded and your vison blurred\nThe only thing you know for sure is your name is " + currentPlayer.name);
-            Console.WriteLine(emptyBreak);
-            Console.WriteLine(lineBreak);
+            Console.WriteLine(titleCard1);
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard3);
+            Console.WriteLine(titleCard4);
+            Console.WriteLine(titleCard5);
+            Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||  You awaken on the cold floor of a large banquet hall. ||");
+            Console.WriteLine("||       Your head is clouded and your vison blurred.     ||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard5);
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine(titleCard);
-            Console.WriteLine(lineBreak);
-            Console.WriteLine("A choice is to be made...  ");
-            Console.WriteLine("Ahead of you is 2 halls.");
-            Console.WriteLine("On the left is a dark hall with candles barely illuminating a never-ending hall.");
-            Console.WriteLine("On the right is a door with swords crossed overhead and what appears to be blood splater across the door.");
-            Console.WriteLine("Which route do you take...");
-            Console.WriteLine(emptyBreak);
-            Console.WriteLine(lineBreak);
+            Console.WriteLine(titleCard1);
+            Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||     A choice is to be made. Ahead of you is 2 halls.   ||");
+            Console.WriteLine(titleCard5);
+            Console.WriteLine(titleCard1);
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||      On the left is a dark hall with candles barely    ||");
+            Console.WriteLine("||            illuminating a never-ending hall.           ||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard5);
+            Console.WriteLine(titleCard1);
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||   On the right is a door with swords crossed overhead  ||");
+            Console.WriteLine("||    what appears to be blood splater across the door.   ||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard5);
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(titleCard1);
+            Console.WriteLine(titleCard2);
+            Console.WriteLine("||                 Which route do you take...             ||");
+            Console.WriteLine(titleCard2);
+            Console.WriteLine(titleCard5);
             String? choice1 = Console.ReadLine();
             Console.Clear();
 
             if(choice1 != null && choice1 == "right")
             {    
-                Console.WriteLine("You reach out for the cold steel of the handle on the door and tug.");
+                Console.WriteLine(titleCard1);
+                Console.WriteLine(titleCard3);
+                Console.WriteLine(titleCard5);
+                Console.WriteLine(titleCard2);
+                Console.WriteLine("||        You reach out for the cold steel of the         ||");
+                Console.WriteLine("||                 handle on the door.                    ||");
                 if(currentPlayer.keys >= 1)
                 {
-                    Console.WriteLine(titleCard);
-                    Console.WriteLine(lineBreak);
+                    
                     Console.WriteLine("You insert the key you found into the door and turn...You hear a click...");
-                    Console.WriteLine(emptyBreak);
-                    Console.WriteLine(lineBreak);
+                    
                     Console.ReadKey();
                     Console.Clear();
                 }
                 else
                 {
                     Console.Clear();
-                    Console.WriteLine(titleCard);
-                    Console.WriteLine(lineBreak);
+                    
                     Console.WriteLine("You see a keyhole , but currently have no keys...");
-                    Console.WriteLine(emptyBreak);
-                    Console.WriteLine(lineBreak);
+                    
                     Console.ReadKey();
                     Console.Clear();
 
@@ -80,12 +108,10 @@ namespace HallsOfWestWood
             else if(choice1 != null && choice1.ToLower() == "left")
             {
                 Console.Clear();
-                Console.WriteLine(titleCard);
-                Console.WriteLine(lineBreak);
+                
                 Console.WriteLine("You slowly begin walking down the hall.");
                 Console.WriteLine("a creaking noise ahead causes the hair to raise on your neck... It's about to get hairy...");
-                Console.WriteLine(emptyBreak);
-                Console.WriteLine(lineBreak);
+                
                 Console.ReadKey();
                 Console.Clear();
                 Encounters.hallFight();
