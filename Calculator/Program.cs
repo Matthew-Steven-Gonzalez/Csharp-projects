ddf59927ac87748b73091c0e85646fbc7e5109a3
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-10 08:33:47 -0400

    added default case for operation selection.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,61 +9,62 @@ namespace Calculator
         public static void Main(string[] arg)
         {
 
-            bool confirmation = false;
 
-            string operation;
+            string operation, confirmation = "yes";
 
             int value1, value2, total;
 
-
-            while(confirmation = false)
-            {
             Console.WriteLine("Hello and welcome to calculator. Here we can do basic addtion, subtraction, multiplication, and diviison with whole numbers.");
-            Console.WriteLine("Please enter your first number:");
-            value1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your second number:");
-            value2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("What kind of math do you want to do?(add , sub, multi, div)");
-            operation = Console.ReadLine().ToLower();
 
-            // while(operation != "add" || operation != "sub" || operation != "multi" || operation != "div")
-            // {
-            //     Console.WriteLine("Please select proper answer. (add, sub, multi, div)");
-            //     operation = Console.ReadLine().ToLower();
-
-            // }
-
-            switch(operation)
+            while(confirmation == "yes" || confirmation == "y" || confirmation == "sure")
             {
-                case "add":
-                    total = value1 + value2;
-                    Console.WriteLine("{0} + {1} = {2}", value1, value2, total);
-                    Console.WriteLine("Do you want to math again?");
-                    confirmation = Convert.ToBoolean(Console.ReadLine());
-                    break;
-                
-                case "sub":
-                    total = value1 - value2;
-                    Console.WriteLine("{0} - {1} = {2}", value1, value2, total);
-                    Console.WriteLine("Do you want to math again?");
-                    confirmation = Convert.ToBoolean(Console.ReadLine());
-                    break;
+                Console.WriteLine("Please enter your first number:");
+                value1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter your second number:");
+                value2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("What kind of math do you want to do?(add , sub, multi, div)");
+                operation = Console.ReadLine().ToLower();
 
-                case "multi":
-                    total = value1 * value2;
-                    Console.WriteLine("{0} x {1} = {2}", value1, value2, total);
-                    Console.WriteLine("Do you want to math again?");
-                    confirmation = Convert.ToBoolean(Console.ReadLine());
-                    break;
 
-                case "div":
-                    total = value1 / value2;
-                    Console.WriteLine("{0} / {1} = {2}", value1, value2, total);
-                    Console.WriteLine("Do you want to math again?");
-                    confirmation = Convert.ToBoolean(Console.ReadLine());
-                    break;
+                switch(operation)
+                {
+                    case "add":
+                        total = value1 + value2;
+                        Console.WriteLine("{0} + {1} = {2}", value1, value2, total);
+                        Console.WriteLine("Do you want to math again?");
+                        confirmation = Console.ReadLine().ToLower();
+                        break;
+                    
+                    case "sub":
+                        total = value1 - value2;
+                        Console.WriteLine("{0} - {1} = {2}", value1, value2, total);
+                        Console.WriteLine("Do you want to math again?");
+                        confirmation = Console.ReadLine().ToLower();
+                        break;
+
+                    case "multi":
+                        total = value1 * value2;
+                        Console.WriteLine("{0} x {1} = {2}", value1, value2, total);
+                        Console.WriteLine("Do you want to math again?");
+                        confirmation = Console.ReadLine().ToLower();
+                        break;
+
+                    case "div":
+                        total = value1 / value2;
+                        Console.WriteLine("{0} / {1} = {2}", value1, value2, total);
+                        Console.WriteLine("Do you want to math again?");
+                        confirmation = Console.ReadLine().ToLower();
+                        break;
+
+                    default:
+                        Console.WriteLine("Your input doesn't match our math functions. Sorry.");
+                        confirmation = "yes";
+                        break;
+
+                }
             }
-        }
+            Console.WriteLine("Thank you for using calculator.");
+
         }
 
     }
