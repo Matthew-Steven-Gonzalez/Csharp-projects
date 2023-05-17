d1a2c21ae43ac97217c397f3467bd3a0dcdfde09
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-17 03:56:47 -0400

    Completed time challenge.

---
+++ TimeChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeChallenge
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time, hours, minuets;
+
+            Console.WriteLine("Please Enter total minutes and TIMe will update to hours and minuets.");
+
+            time = Int32.Parse(Console.ReadLine());
+
+            hours = time / 60;
+
+            minuets = time % 60;
+
+            Console.WriteLine(String.Format("TIMe: time converted Hours: {0} and Minutes:{1}. ",hours, minuets));
+        }
+    }
+}
