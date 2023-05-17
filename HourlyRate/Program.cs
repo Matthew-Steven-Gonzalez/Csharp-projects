66918c1f6f9b4547fc5e48188eeef0e7f3240098
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-17 04:36:06 -0400

    completed HourlyRate challenge.

---
+++ HourlyRate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an hourly rate");
+
+            double rate = double.Parse(Console.ReadLine());
+
+            if (rate > 7.50 && rate < 49.00)
+            {
+                Console.WriteLine("That rate is okay.");
+            }
+            else
+            {
+                Console.WriteLine("Error with that pay rate.");
+            }
+        }
+    }
+}
