158919cecc7ed3424d7475c2e71d165ecfabf0cd
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-17 13:30:22 -0400

    completed a do while loop exercise. PayrateLoop.

---
+++ PayrateLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrateLoop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double rate;
+
+            do
+            {
+            Console.WriteLine("Please enter an hourly rate");
+            rate = double.Parse(Console.ReadLine());
+            }while(rate < 5.56 || rate > 49.99);
+
+                Console.WriteLine("The rate of {0} is okay.", rate);
+                        
+        }
+    }
+}
