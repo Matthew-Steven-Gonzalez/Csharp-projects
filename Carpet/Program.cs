ad29214e5edf29184ad2543c1dc6592789401a91
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-17 03:46:09 -0400

    added Carpet Exercise 2 from UDemy course.

---
+++ Carpet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int length;
+            int width;
+            const float price = 4.56f;
+            
+            Console.WriteLine("Please enter the length of the room:");
+            length = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Please enter the width of the room:");
+            width = int.Parse(Console.ReadLine());
+            Console.Clear();
+            
+
+            int roomArea = width * length;
+
+            float total =  price * (float) roomArea;
+
+            Console.WriteLine(String.Format("Your total ammount to carpet your room is: ${0:0.##}", total));
+
+        }
+    }
+}
