8bad3ab3cd4dd28aff388f5016a5e6fa9aff3dfb
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-18 11:17:38 -0400

    added second solution for ZipcodeDeliver

--- ZipcodeDeliver/Program.cs
+++ ZipcodeDeliver/Program.cs
@@ -11,21 +11,50 @@ namespace ZipcodeDeliver
         public static void Main(string[] args)
         {
 
-            int[] zipWeDo = new int[]{23451,77654,93847,16253,16253,74635,84756,83627,91029,00202};
+            // int[] zipWeDo = new int[]{23451,77654,93847,16253,16253,74635,84756,83627,91029,00202};
 
-            Console.WriteLine("Please enter a zipCode :");
-            int customZip = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Please enter a zipCode :");
+            // int customZip = int.Parse(Console.ReadLine());
 
             
-                if(zipWeDo.Contains(customZip))
+            //     if(zipWeDo.Contains(customZip))
+            //     {
+            //         Console.WriteLine("We do Deliver there.");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("We do NOT Deliver there.");
+            //     }
+            //     Console.WriteLine("Thank you...");
+
+
+                // Course Solution: 
+
+            string[] zipWeDo = new string[]{"23451","77654","93847","16253","16253","74635","84756","83627","91029","00202"};
+
+
+            Console.WriteLine("Please enter a zipCode :");
+            string code = Console.ReadLine();
+            bool found = false;
+
+            for(int i = 0; i <zipWeDo.Length;i++)
+            {
+                if(zipWeDo[i] == code)
                 {
-                    Console.WriteLine("We do Deliver there.");
+                    found = true;
+                    break;
                 }
-                else
-                {
-                    Console.WriteLine("We do NOT Deliver there.");
-                }
-                Console.WriteLine("Thank you...");
+            }
+
+            if(found)
+            {
+                Console.WriteLine("We do deliver there.");
+            }
+            else
+            {
+                Console.WriteLine("We do NOT deliver there");
+            }
+
         }
     }
 }
