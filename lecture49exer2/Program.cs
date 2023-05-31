01989dc6056b4ba0cd129771959b0cccc66b5eef
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-31 14:31:22 -0400

    added lecture49exer2 completed.

---
+++ lecture49exer2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer for it to be cubed and sqaured.");
+            int userNum = int.Parse(Console.ReadLine());
+            int userNumSquared = squared(userNum);
+            Console.WriteLine(userNumSquared);
+            int userNumCubed = cubed(userNum);
+            Console.WriteLine(userNumCubed);
+
+        }
+
+        public static int squared(int x)
+        {
+            return x * x;
+        }
+
+        public static int cubed(int x)
+        {
+            return squared(x) * x;
+        }
+    }
+}
