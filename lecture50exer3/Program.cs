aa61ec1e2b362f1b744ca3c09f6e6337b88d176c
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-06-01 07:30:16 -0400

    lecture50exer3 completed added.

---
+++ lecture50exer3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(2,3,4,5);
+            Sum(3, 4);
+
+        }
+
+        public static void Sum(params int[] nums)
+        {
+            int sum = 0;
+
+            for(int i = 0;i < nums.Length;i++)
+            {
+                sum += nums[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
