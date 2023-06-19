ee30b624f0923e2c7d832daaadd525b5434fd3c4
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-06-18 21:43:41 -0400

    added daily challenge

---
+++ SquareSum/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    public static int SquareSum(int[] numbers)
+    { 
+        int sum = 0;
+        for(int i = 0; i < numbers.Length;i++)
+        {
+            sum += (numbers[i] * numbers[i]);
+        }
+        return sum;
+    }
+}
