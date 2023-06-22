ec64c71d011777d9c2948134e74e0a0481987252
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-06-21 22:44:18 -0400

    update

--- StringsMethods/Program.cs
+++ StringsMethods/Program.cs
@@ -29,11 +29,14 @@ namespace StringMethods
             //String and its methods 2
 
             Console.WriteLine("Enter a string here: ");
+            //user input string
             string userString = Console.ReadLine();
 
             Console.WriteLine("Enter Character To search: ");
-            string searchedLetter = Console.ReadLine();
+            //user searched letter char
+            char searchedLetter = Console.ReadLine();
 
+            //if else statment for if character is present.
             if(userString.Contains(searchedLetter))
             {
                 Console.WriteLine("Your letter is there its occurs first at {0}." , userString.IndexOf(searchedLetter));
@@ -61,6 +64,25 @@ namespace StringMethods
             {
                 Console.WriteLine("Cheer up I hope you feel better.");
             }
+
+
+
+            //datatype challenge
+            byte value1 = 1;
+            sbyte value2 = 1;
+            int value3 = 1;
+            uint value4 = 1;
+            short value5 = 1;
+            ushort value6 = 1;
+            long value7 = 1;
+            ulong value8 = 1;
+            float value9 = 1.0f;
+            double value10 = 1.2;
+            char value11 = "a";
+            bool value12 = true;
+            object value13;
+            string value14 = "I control Text";
+            decimal value15 = 2.4m;
 
 
         }
